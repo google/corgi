4c46bccb0a724cede6e3f3d3938e3f145dae5a12
Stewart Miles <smiles@google.com>
2016-06-07 13:18:26 -0700

    Switch from the deprecated gnustl runtime to libc++.
    NOTE: This results in spurious warnings of the form
    "LOCAL_LDLIBS is always ignored for static libraries" due to the
    use of libc++.
    
    Change-Id: Id18560991657fd7256c28b8c2cd7004e801e0ea5

--- jni/Application.mk
+++ jni/Application.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-APP_STL:=gnustl_static
+APP_STL:=c++_static
 APP_ABI := armeabi-v7a x86 armeabi
 
 APP_CPPFLAGS += -std=c++11 -Wno-literal-suffix
--- sample/jni/Application.mk
+++ sample/jni/Application.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 APP_PLATFORM := android-9
 APP_ABI:=armeabi armeabi-v7a mips x86 x86_64
-APP_STL := gnustl_static
+APP_STL:=c++_static
 APP_MODULES := entity_component_system_sample
 
 APP_CPPFLAGS += -std=c++11 -Wno-literal-suffix
