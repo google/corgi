318912db22d0d8b65497c7ce2a1cf154c3a59178
jsanmiya <jsanmiya@google.com>
2017-01-27 21:27:31 +0000

    Fix Android builds for Pindrop, Scene Lab, CORGI, and Breadboard. PiperOrigin-RevId: 145830004 Change-Id: I07e4fa1ecb72729e7ccecc9b17efa62fd44c37de

--- jni/android_config.mk
+++ jni/android_config.mk
@@ -12,25 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Locations of 3rd party and FPL libraries.
-FPL_ROOT:=$(CORGI_DIR)/../../libs
-# If the dependencies directory exists either as a subdirectory or as the
-# container of this project directory, assume the dependencies directory is
-# the root directory for all libraries required by this project.
-$(foreach dep_dir,$(wildcard $(CORGI_DIR)/dependencies) \
-                  $(wildcard $(CORGI_DIR)/../../dependencies),\
-  $(eval DEPENDENCIES_ROOT?=$(dep_dir)))
-ifneq ($(DEPENDENCIES_ROOT),)
-  THIRD_PARTY_ROOT:=$(DEPENDENCIES_ROOT)
-  FPL_ROOT:=$(DEPENDENCIES_ROOT)
-else
-  THIRD_PARTY_ROOT:=$(FPL_ROOT)/../../../external
-endif
-
-# Location of the CORGI library.
-# This intentionally overrides any prior setting.
-# NOTE: This is only required as the CORGI's component library and Breadboard's
-# module library have a circular dependency.
-DEPENDENCIES_CORGI_DIR:=$(CORGI_DIR)
-# Location of the fplutil library (only for sample code).
-DEPENDENCIES_FPLUTIL_DIR:=$(FPL_ROOT)/fplutil
+include $(call my-dir)/find_fplutil.mk
+include $(FPLUTIL_DIR)/buildutil/android_common.mk
