582fe08976e7e8dad2b0e13c175f2a510f1c9a2d
jsanmiya <jsanmiya@google.com>
2017-02-14 20:31:13 +0000

    Build modifications to get Zooshi to build working.
    With these changes, Zooshi will build with Blaze, CMake, and Android ndk-build.
    PiperOrigin-RevId: 147506488
    Change-Id: Ib42d6de04bdd1a785b9420ac6c034e7a66170572

--- component_library/jni/bullet/Android.mk
+++ component_library/jni/bullet/Android.mk
@@ -19,37 +19,32 @@ CORGI_COMPONENT_LIBRARY_DIR:=$(LOCAL_PATH)/../..
 include $(CORGI_COMPONENT_LIBRARY_DIR)/jni/android_config.mk
 
 include $(CLEAR_VARS)
-LOCAL_PATH:=$(DEPENDENCIES_BULLETPHYSICS_DIR)/src
+LOCAL_PATH:=$(DEPENDENCIES_BULLETPHYSICS_DIR)
 
 LOCAL_MODULE := libbullet
 LOCAL_MODULE_FILENAME := libbullet
 
 LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/ \
-    $(LOCAL_PATH)/BulletCollision/BroadphaseCollision \
-    $(LOCAL_PATH)/BulletCollision/CollisionDispatch \
-    $(LOCAL_PATH)/BulletCollision/CollisionShapes \
-    $(LOCAL_PATH)/BulletCollision/NarrowPhaseCollision \
-    $(LOCAL_PATH)/BulletDynamics/ConstraintSolver \
-    $(LOCAL_PATH)/BulletDynamics/Dynamics \
-    $(LOCAL_PATH)/BulletDynamics/Vehicle \
-    $(LOCAL_PATH)/LinearMath \
+    $(LOCAL_PATH)/src/ \
+    $(LOCAL_PATH)/src/BulletCollision/BroadphaseCollision \
+    $(LOCAL_PATH)/src/BulletCollision/CollisionDispatch \
+    $(LOCAL_PATH)/src/BulletCollision/CollisionShapes \
+    $(LOCAL_PATH)/src/BulletCollision/NarrowPhaseCollision \
+    $(LOCAL_PATH)/src/BulletDynamics/ConstraintSolver \
+    $(LOCAL_PATH)/src/BulletDynamics/Dynamics \
+    $(LOCAL_PATH)/src/BulletDynamics/Vehicle \
+    $(LOCAL_PATH)/src/LinearMath \
+    $(LOCAL_PATH)/Extras/VHACD/inc
 
 LOCAL_ARM_MODE := arm
 
 bullet_cpp_in_subdirectory = \
   $(call fplutil_all_files_relative_to_path_in_subdirectory,$(LOCAL_PATH),$(1),cpp)
 
-BULLET_COLLISION_SRC_FILES := \
-    $(call bullet_cpp_in_subdirectory,BulletCollision)
-BULLET_DYNAMICS_SRC_FILES := \
-    $(call bullet_cpp_in_subdirectory,BulletDynamics)
-BULLET_LINEARMATH_SRC_FILES := \
-    $(call bullet_cpp_in_subdirectory,LinearMath)
-
 LOCAL_SRC_FILES := \
-    $(BULLET_COLLISION_SRC_FILES) \
-    $(BULLET_DYNAMICS_SRC_FILES) \
-    $(BULLET_LINEARMATH_SRC_FILES)
+    $(call bullet_cpp_in_subdirectory,src/BulletCollision) \
+    $(call bullet_cpp_in_subdirectory,src/BulletDynamics) \
+    $(call bullet_cpp_in_subdirectory,src/LinearMath) \
+    $(call bullet_cpp_in_subdirectory,Extras/VHACD)
 
 include $(BUILD_STATIC_LIBRARY)
